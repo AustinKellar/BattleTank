a7a7e863bc38746422ccee18eafad8a28c4e7e86
Austin Kellar <akellar@ucsc.edu>
2019-09-22 11:11:57 -0700

    redesigned the tank movement code such that it only applies force while grounded

--- Source/BattleTank/Private/TankTrack.cpp
+++ Source/BattleTank/Private/TankTrack.cpp
@@ -5,7 +5,7 @@
 
 UTankTrack::UTankTrack()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UTankTrack::BeginPlay()
@@ -14,27 +14,30 @@ void UTankTrack::BeginPlay()
 	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
 }
 
-void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	PreventSlippage(DeltaTime);
-}
-
 void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
 {
 	UE_LOG(LogTemp, Warning, TEXT("%s hit!"), *GetName());
+	DriveTrack();
+	PreventSlippage();
+	CurrentThrottle = 0.0f;
 }
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	FVector ForceApplied = GetForwardVector() * Throttle * MaxDrivingForce;
+	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
+}
+
+void UTankTrack::DriveTrack()
+{
+	FVector ForceApplied = GetForwardVector() * CurrentThrottle * MaxDrivingForce;
 	FVector ForceLocation = GetComponentLocation();
 	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
-void UTankTrack::PreventSlippage(float DeltaTime)
+void UTankTrack::PreventSlippage()
 {
+	float DeltaTime = GetWorld()->GetDeltaSeconds();
 	UStaticMeshComponent* TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
 	float SlippageSpeed = FVector::DotProduct(GetComponentVelocity(), GetRightVector());
 	FVector CorrectionAcceleration = (SlippageSpeed / DeltaTime) * -GetRightVector();
--- Source/BattleTank/Public/TankTrack.h
+++ Source/BattleTank/Public/TankTrack.h
@@ -24,14 +24,16 @@ private:
 	UPROPERTY(EditDefaultsOnly, Category = "Driving")
 	float MaxDrivingForce = 25000000; // Assume 40 ton tank and 1g acceleration
 
+	float CurrentThrottle = 0.0f;
+
 	// Engine Methods
 	UTankTrack();
 	virtual void BeginPlay() override;
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	// Custom Methods
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 
-	void PreventSlippage(float DeltaTime);
+	void DriveTrack();
+	void PreventSlippage();
 };
